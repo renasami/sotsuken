7f92c632b71f4f2e2f05c699585d124eb9fea823
ren <drrr0502@gmail.com>
2022-05-29 16:20:38 +0900

    delete some files

--- crawl/util/requests.py
+++ crawl/util/requests.py
@@ -1,5 +1,6 @@
 import requests
 from requests import Response 
+import asyncio
 
 class Requests:
     def __init__(self,url:str,key:str):
@@ -23,7 +24,11 @@ class YahooRequest(Requests):
     def __init__(self,url,key):
         self.url = url
         self.key = "/?appid={}".format(key)
-        print(self)
+
+    
+    def __get(self) -> Response:  
+        res = requests.get(self.url + self.key)
+        return res
 
     async def __get_with_param(self,param) -> Response:
         keys = list(param.keys())
@@ -34,14 +39,16 @@ class YahooRequest(Requests):
         return res 
 
     async def get_with_params(self)->list:
-        total = await self.__get(self).total
+        total = await self.__get()
+        print(total)
         params = {
             "results":100,
             "starts": 1
         }
         result = []
         while total > 0:
-            resp = await self.__get_with_param(self,params)
+            resp = await asyncio.wait_for(self.__get_with_param(self,params))
+
             for n in resp.json()["results"]:
                 result.append(n)
             total -= 100
--- crawl/yahoo_api.py
+++ crawl/yahoo_api.py
@@ -1,11 +1,14 @@
-from urllib.request import Request
+from requests import Request
 import requests
 import csv 
+import asyncio
 
 from config import YAHOO_CLIENT_KEY 
-from util.requests import YahooRequest,Requests
+from util.requests import YahooRequest
+
 async def get_company():
     yahoo_request = YahooRequest(url="https://job.yahooapis.jp/v1/furusato/company",key=YAHOO_CLIENT_KEY)
+
     with open('test.csv', 'w') as csv_file:
         fieldnames = ['name', 'prefecture','city','town','block','building','presidentPosition','presidentName','capital','employees','establishmentDate','listed','stockCode','averageAge','femaleRate','averageAnnualIncome','paidHolidayDigestibility','turnoverRate','handicappedEmployeeRate','femaleManagerRate','averageDuration','sales','salesDate','currentEarnings','currentEarningsDate','webUrl','prText','remarks']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -17,4 +20,5 @@ async def get_company():
                 dic[s] = n[s]
             writer.writerow(dic)
 
-get_company()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_company())
